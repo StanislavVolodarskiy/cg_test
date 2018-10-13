9230a73ed4de422947a8b85c77479fe94ee7e3a4
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2018-10-14 01:20:04 +0300

    initial commit

---
+++ cg_test.py
@@ -0,0 +1,145 @@
+import argparse
+import contextlib
+import gc
+import os
+import sys
+import time
+
+
+class BaseTask(object):
+    def __init__(self, name):
+        self.name = name
+
+    def size(self):
+        """Return human readable task size."""
+        raise NotImplementedError
+
+    def complexity(self):
+        """Return expected task time complexity."""
+        raise NotImplementedError
+
+    def write_task_to_file(self, f):
+        """Write task to file."""
+        raise NotImplementedError
+
+    def read_answer_from_file(self, f):
+        """Read answer from file."""
+        raise NotImplementedError
+
+    def check_answer(self, answer):
+        """Check answer for correctness."""
+        raise NotImplementedError
+
+    def performance(self, duration):
+        """Return human readable performance data."""
+        size = self.size()
+        complexity = self.complexity()
+        return 'T({}) / {:.3f} = {:.3g}'.format(size, duration, complexity / duration)
+
+    def write_task(self):
+        """Write task for computation."""
+        with open('tmp/in', 'w') as f:
+            self.write_task_to_file(f)
+
+    @staticmethod
+    def compute_answer():
+        """Compute answer for task."""
+        os.system('./run')
+
+    def read_answer(self):
+        """Read computed answer."""
+        with open('tmp/out', 'r') as f:
+            return self.read_answer_from_file(f)
+
+    @staticmethod
+    def elapsed_time(f):
+        start = time.time()
+        result = f()
+        finish = time.time()
+        return result, finish - start
+
+    @staticmethod
+    def fail(what):
+        raise Exception(what)
+
+
+class Runner(object):
+    def __init__(self, task_factory):
+        self._task_factory = task_factory
+        self._tasks = []
+
+    def run(self, arg1, *args):
+        if isinstance(arg1, basestring):
+            assert len(args) == 1
+            name = arg1
+            task_data = args[0]
+            self._append(name, lambda: task_data, ())
+        else:
+            f = arg1
+            self._append(f.__name__, f, args)
+
+    def main(self):
+        parser = argparse.ArgumentParser(description='Run tests')
+        parser.add_argument('-l', '--list', action='store_true', help='list tests')
+        parser.add_argument('prefixes', metavar='TEST_PREFIX', nargs='*', type=str)
+        args = parser.parse_args()
+
+        if args.list:
+            for n, _ in self._select_tasks(args.prefixes):
+                print n
+            sys.exit(0)
+
+        for n, f in self._select_tasks(args.prefixes):
+            self._run_task(lambda: self._task_factory(n, f()))
+        sys.exit(0)
+
+    def _append(self, name, f, args):
+        ff = f
+        if len(args) > 0:
+            name = '_'.join([name] + map(str, args))
+            def ff():
+                return f(*args)
+        self._tasks.append((name, ff)) 
+
+    def _select_tasks(self, prefixes):
+        if len(prefixes) == 0:
+            prefixes = ['']
+        for n, f in self._tasks:
+            for p in prefixes:
+                if n.startswith(p):
+                    yield n, f
+                    break
+
+    def _run_task(self, task_factory):
+        """Run task, check answer and report result."""
+
+        task = task_factory()
+
+        print task.name,
+        sys.stdout.flush()
+        task.write_task()
+
+        task = None
+        gc.collect()
+
+        # start testing program
+        _, elapsed = self._task_factory.elapsed_time(self._task_factory.compute_answer)
+
+        task = task_factory()
+
+        print task.performance(elapsed),
+        sys.stdout.flush()
+
+        answer = task.read_answer()
+
+        task.check_answer(answer)
+
+        task = None
+        gc.collect()
+
+
+@contextlib.contextmanager
+def runner(task_factory):
+    runner = Runner(task_factory)
+    yield runner.run
+    runner.main()
---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='cg_test',
+    version='0.1',
+    packages=['cg_test']
+)
